1abd15252f157c57401a549fcd36a8e9a79100c5
AdityaDotZip <69252834+AdityaDotZip@users.noreply.github.com>
2020-10-01 12:11:37 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,105 @@
+class Game {
+  constructor(){
+
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+
+    car1 = createSprite(100,200);
+    car1.addImage(carImg1);
+    car2 = createSprite(300,200);
+    car2.addImage(carImg2);
+    car3 = createSprite(500,200);
+    car3.addImage(carImg3);
+    car4 = createSprite(700,200);
+    car4.addImage(carImg4);
+    cars = [car1, car2, car3, car4];
+  }
+
+  play(){
+    form.hide();
+
+    Player.getPlayerInfo();
+    Player.getCarsAtEnd();
+    
+    if(allPlayers !== undefined){
+      background(groundImg);
+      image(trackImg, 0, -displayHeight*4, displayWidth, displayHeight*5);
+
+      var index = 0;
+
+      var x = 175;
+      var y;
+
+      for(var plr in allPlayers){
+      
+        index = index + 1 ;
+
+        x = x + 200;
+      
+        y = displayHeight - allPlayers[plr].distance;
+        cars[index-1].x = x;
+        cars[index-1].y = y;
+
+        if (index === player.index){
+          stroke(10);
+          fill("red");
+          ellipse(x, y, 60 ,60);
+          cars[index - 1].shapeColor = "red";
+          camera.position.x = displayWidth/2;
+          camera.position.y = cars[index-1].y
+        }
+       
+      }
+
+    }
+    if(player.distance > 3860){
+      gameState = 2;
+      this.update(gameState);
+         player.rank+=1;
+      Player.updateCarsAtEnd(player.rank);
+    }
+
+    /*if(gameState !== 2 && player.distance> 3859){
+      player.rank+=1;
+      Player.updateCarsAtEnd(player.rank);
+    }*/
+
+    if(keyIsDown(UP_ARROW) && player.index !== null && gameState !== 2){
+      player.distance +=10
+      player.update();
+    }
+
+    drawSprites();
+  }
+
+  
+  end(){
+    console.log("Game Ended");
+    console.log("Your Rank: "+ player.rank);
+  }
+}
